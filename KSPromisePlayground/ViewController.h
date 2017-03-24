543a2e07e70187232b77f4f1d91f6dc094acf0b7
Lottis, Kent <Kent.D.Lottis@nordstrom.com>
2017-03-24 13:10:05 -0700

    Initial Commit

---
+++ KSPromisePlayground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KSPromisePlayground
+//
+//  Created by Lottis, Kent on 3/24/17.
+//  Copyright © 2017 Lottis, Kent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KSPromisePlayground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KSPromisePlayground
+//
+//  Created by Lottis, Kent on 3/24/17.
+//  Copyright © 2017 Lottis, Kent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
