b13ffd211464d395c1575c65a59e53e40216fa0b
Lottis, Kent <Kent.D.Lottis@nordstrom.com>
2017-03-24 15:03:47 -0700

    Baseline scenarios: model the current broken array-based model and a

--- KSPromisePlaygroundTests/KSPromisePlaygroundTests.m
+++ KSPromisePlaygroundTests/KSPromisePlaygroundTests.m
@@ -9,8 +9,91 @@
 #import <XCTest/XCTest.h>
 #import "KSDeferred.h"
 
-@interface KSPromisePlaygroundTests : XCTestCase
+@protocol PromiseMonster
+-(KSPromise *) promiseForMonster;
+-(void) pokeMonster: (NSString *) monster;
+@end
 
+@interface CoolMonster: NSObject<PromiseMonster>
+@property (nonatomic, strong) KSDeferred *currentDeferred;
+@property (nonatomic, strong) KSDeferred *dependentDefer;
+@end
+
+@interface BrokenMonster: NSObject<PromiseMonster>
+@property (nonatomic, strong) NSMutableArray<KSDeferred *> *currentDeferreds;
+@property (nonatomic, strong) KSDeferred *dependentDefer;
+@end
+
+@implementation BrokenMonster
+-(instancetype) init {
+    self = [super init];
+    if (self) {
+        _currentDeferreds = [NSMutableArray array];
+        _dependentDefer = [KSDeferred defer];
+    }
+    return self;
+}
+
+-(KSPromise *) promiseForMonster {
+    KSDeferred *defer = [KSDeferred defer];
+
+    [self.currentDeferreds addObject:defer];
+    if (self.currentDeferreds.count > 1) {
+        [self.dependentDefer.promise then:^id (NSString * value) {
+            NSLog(@"Monster resolving: k: %lu", self.currentDeferreds.count);
+            for (KSDeferred *defer in self.currentDeferreds) {
+                [defer resolveWithValue:value];
+            }
+            NSLog(@"Monster resolved: k: %lu", self.currentDeferreds.count);
+            [self.currentDeferreds removeAllObjects];
+            NSLog(@"Monster cleared: k: %lu", self.currentDeferreds.count);
+            return nil;
+        }];
+    }
+
+    return defer.promise;
+}
+
+-(void) pokeMonster: (NSString *) monster {
+    [self.dependentDefer resolveWithValue:monster];
+}
+
+@end
+
+@implementation CoolMonster
+-(instancetype) init {
+    self = [super init];
+    if (self) {
+        _dependentDefer = [KSDeferred defer];
+    }
+    return self;
+}
+
+-(KSPromise *) promiseForMonster {
+    if (self.currentDeferred) {
+        return self.currentDeferred.promise;
+    }
+
+    KSDeferred *defer = [KSDeferred defer];
+    self.currentDeferred = defer;
+        [self.dependentDefer.promise then:^id (NSString * value) {
+            NSLog(@"Cool-monste then");
+            [self.currentDeferred resolveWithValue:value];
+            self.currentDeferred = nil;
+            NSLog(@"Cool-monster cleared");
+            return nil;
+        }];
+
+    return defer.promise;
+}
+
+-(void) pokeMonster: (NSString *) monster {
+    [self.dependentDefer resolveWithValue:monster];
+}
+
+@end
+
+@interface KSPromisePlaygroundTests : XCTestCase
 @end
 
 @implementation KSPromisePlaygroundTests
@@ -30,6 +113,77 @@
     [target resolveWithValue:@"Kent"];
     XCTAssertEqual(target.promise.value, @"Kent");
 
+}
+
+- (void)testReentrancy {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"promise"];
+
+    KSDeferred *defer = [KSDeferred defer];
+    KSPromise *target = defer.promise;
+    [target then:^id (NSString * value) {
+        NSLog(@"Level 1");
+        XCTAssertEqual(value, @"Freddie");
+
+        // now, in the promise completion completion block, somebody calls back into the same KSDeferred
+        [target then:^id (NSString * value) {
+            NSLog(@"Level 2");
+            XCTAssertEqual(value, @"Freddie");
+            // rentrant goop
+            [expectation fulfill];
+            return nil;
+        }];
+
+        return nil;
+    }];
+    [defer resolveWithValue:@"Freddie"];
+
+    [self waitForExpectationsWithTimeout:1 handler:nil];
+}
+
+- (void) testBrokenMonster {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"promise"];
+
+    BrokenMonster *monster = [[BrokenMonster alloc] init];
+    [monster.promiseForMonster then:^id (NSString * value) {
+        NSLog(@"Monster 1");
+        XCTAssertEqual(value, @"Froggie");
+
+        [monster.promiseForMonster then:^id (NSString * value) {
+            NSLog(@"Monster 2");
+            XCTAssertEqual(value, @"Froggie");
+            [expectation fulfill];
+            return nil;
+        }];
+        
+        return nil;
+    }];
+
+    [monster pokeMonster:@"Froggie"];
+    [self waitForExpectationsWithTimeout:1 handler:nil];
+
+}
+
+- (void) testCoolMonster {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"promise"];
+
+    CoolMonster *monster = [[CoolMonster alloc] init];
+    [monster.promiseForMonster then:^id (NSString * value) {
+        NSLog(@"Monster 1");
+        XCTAssertEqual(value, @"Froggie");
+
+        [monster.promiseForMonster then:^id (NSString * value) {
+            NSLog(@"Monster 2");
+            XCTAssertEqual(value, @"Froggie");
+            [expectation fulfill];
+            return nil;
+        }];
+
+        return nil;
+    }];
+
+    [monster pokeMonster:@"Froggie"];
+    [self waitForExpectationsWithTimeout:1 handler:nil];
+    
 }
 
 @end
