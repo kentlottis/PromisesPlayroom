ae4ad9c882f1557026823b68f023657549e6497b
Lottis, Kent <Kent.D.Lottis@nordstrom.com>
2017-03-24 21:08:27 -0700

    Improve the ImprovedCacheModel class to actually cache the asset

--- KSPromisePlayground/ImprovedCacheModel.m
+++ KSPromisePlayground/ImprovedCacheModel.m
@@ -1,5 +1,11 @@
 #import "ImprovedCacheModel.h"
 
+@interface ImprovedCacheModel()
+
+@property (nonatomic, strong) NSString *cachedAsset;
+
+@end
+
 @implementation ImprovedCacheModel
 -(instancetype) initWithProvider: (id<AssetProviderProtocol>) provider {
     self = [super initWithProvider:provider];
@@ -12,12 +18,18 @@
 -(KSPromise *) promiseForAsset {
     KSDeferred *defer = [KSDeferred defer];
 
+    if (self.cachedAsset) {
+        [defer resolveWithValue:self.cachedAsset];
+        return defer.promise;
+    }
+
     [self.currentDeferreds addObject:defer];
     if (self.currentDeferreds.count > 1) {
         return defer.promise;
     }
 
     [self.provider.promiseForAsset then:^id (NSString * value) {
+        self.cachedAsset = value;
         while (self.currentDeferreds.count > 0) {
             KSDeferred *defer = self.currentDeferreds.firstObject;
             [defer resolveWithValue:value];
